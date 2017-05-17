0f8fcb7e01dc0d8d9c8cd73ec23a20343564183a
Reda EL Fillali <reda.elfillali@gmail.com>
2017-05-17 15:11:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wordpress Security by Reda EL Fillali
+Description: Sécuriser votre wordpress
+Version: 0.1
+Author: Reda El Fillali
+License: GPL2
+*/
+
+// Supprimer la version de Wordpress
+remove_action('wp_head', 'wp_generator');
+?>
